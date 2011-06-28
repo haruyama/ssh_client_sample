84e0e149040fa17e273126d959d15dd64f0de42a
HARUYAMA Seigo <haruyama@unixuser.org>
2011-06-28 12:52:37 +0900

    add a parser of "string"

--- src/main/scala/org/unixuser/haruyama/ssh/parser/ByteParsers.scala
+++ src/main/scala/org/unixuser/haruyama/ssh/parser/ByteParsers.scala
@@ -7,11 +7,15 @@ import org.unixuser.haruyama.ssh.datatype._
 trait ByteParsers extends Parsers {
   type Elem = Byte
 
+  def toStr(bytes: Seq[Byte]) : String = {
+    new String(bytes.toArray)
+  }
 
   lazy val anyElem: Parser[Elem] = elem("anyElem", _ => true)
   lazy val byte : Parser[Byte] = anyElem
-  //lazy val uint32: Parser[UInt32] = repN(4, byte) ^^ UInt32.toUInt32
   lazy val uint32: Parser[UInt32] = repN(4, byte) ^^ UInt32.toUInt32
+  //toInt のために完全に仕様通りではない
+  lazy val string: Parser[String] = uint32 >> (l => repN(l.value.toInt, byte)) ^^ toStr
 
   def parse[T](p: Parser[T], in: Input): ParseResult[T] = p(in)
   def parse[T](p: Parser[T], bytes: Seq[Byte]): ParseResult[T] = parse(p, new ByteReader(bytes))
