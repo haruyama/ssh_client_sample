037ce5ff5be4e6ffccc72d90976712e763c92814
HARUYAMA Seigo <haruyama@unixuser.org>
2011-06-29 21:03:12 +0900

    add SSHNamedList class and its testsuite.

---
+++ src/test/scala/org/unixuser/haruyama/ssh/datatype/SSHNamedListSuite.scala
@@ -0,0 +1,20 @@
+package org.unixuser.haruyama.ssh.datatype
+import org.scalatest.FunSuite
+
+
+class SSHNamedListSuite extends FunSuite {
+
+  test("tobytes") {
+    assert(SSHNamedList(List()).toBytes === Array[Byte](0,0,0,0))
+    assert(SSHNamedList(List("zlib")).toBytes === Array(0,0,0,4, 0x7a, 0x6c, 0x69, 0x62).map(_.toByte))
+    assert(SSHNamedList(List("zlib", "none")).toBytes === Array(0,0,0,9, 0x7a, 0x6c, 0x69, 0x62, 0x2c, 0x6e, 0x6f, 0x6e, 0x65).map(_.toByte))
+  }
+
+  test("no namedlist") {
+    intercept[AssertionError] {
+      SSHNamedList(List("a,"))
+    }
+  }
+
+}
+
