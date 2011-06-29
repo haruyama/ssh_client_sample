cb8bef732306e075ec65e576b727aa379838093d
HARUYAMA Seigo <haruyama@unixuser.org>
2011-06-29 12:49:42 +0900

    convert Array[Byte](..., 0x80.toByte, ...) to Array(..., 0x80, ...).map(_.toByte)

--- src/test/scala/org/unixuser/haruyama/ssh/datatype/SSHUInt32Suite.scala
+++ src/test/scala/org/unixuser/haruyama/ssh/datatype/SSHUInt32Suite.scala
@@ -6,9 +6,9 @@ class SSHUInt32Suite extends FunSuite {
 
   test("tobytes") {
     assert(SSHUInt32(0L).toBytes === Array[Byte](0, 0, 0, 0))
-    assert(SSHUInt32(699921578L).toBytes === Array[Byte](0x29, 0xb7.toByte, 0xf4.toByte, 0xaa.toByte))
-    assert(SSHUInt32(2147483648L).toBytes === Array[Byte](0x80.toByte, 0, 0, 0))
-    assert(SSHUInt32(4294967295L).toBytes === Array[Byte](255.toByte,255.toByte,255.toByte,255.toByte))
+    assert(SSHUInt32(699921578L).toBytes === Array(0x29, 0xb7, 0xf4, 0xaa).map(_.toByte))
+    assert(SSHUInt32(2147483648L).toBytes === Array(0x80, 0, 0, 0).map(_.toByte))
+    assert(SSHUInt32(4294967295L).toBytes === Array(255,255,255,255).map(_.toByte))
   }
 
   test("no uint32") {
