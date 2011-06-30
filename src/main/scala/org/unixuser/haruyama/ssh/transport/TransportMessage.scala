e2d7d08ab35016958baef8be30597d0921c8b7e8
HARUYAMA Seigo <haruyama@unixuser.org>
2011-06-30 17:28:58 +0900

    bug fix & add Newkeys class and its parser.

--- src/main/scala/org/unixuser/haruyama/ssh/transport/TransportMessage.scala
+++ src/main/scala/org/unixuser/haruyama/ssh/transport/TransportMessage.scala
@@ -4,6 +4,7 @@ import org.unixuser.haruyama.ssh.parser._
 import org.unixuser.haruyama.ssh.datatype._
 import scala.util.parsing.combinator._
 import org.unixuser.haruyama.ssh.message.Message
+import java.security.SecureRandom
 
 abstract class TransportMessage extends Message
 
@@ -18,23 +19,78 @@ object TransportConstant {
   val SSH_MSG_NEWKEYS         = SSHByte(21)
 }
 
+case class Newkeys(messageId: SSHByte) extends TransportMessage {
+  assert(messageId == TransportConstant.SSH_MSG_NEWKEYS)
+  override def toBytes() : Array[Byte] = toBytes(this.productIterator)
+}
+
 case class ServiceRequest(messageId: SSHByte, serviceName : SSHString) extends TransportMessage {
   assert(messageId == TransportConstant.SSH_MSG_SERVICE_REQUEST)
   override def toBytes() : Array[Byte] = toBytes(this.productIterator)
 }
 
+case class Kexinit(messageId: SSHByte, cookie: Seq[SSHByte], kexAlgo: SSHNamedList,
+  serverHostKeyAlgo: SSHNamedList, encC2S: SSHNamedList,
+  encS2C: SSHNamedList, macC2S: SSHNamedList, macS2C : SSHNamedList,
+  compC2S: SSHNamedList, compS2C: SSHNamedList, langC2S: SSHNamedList,
+  langS2C: SSHNamedList, firstKexPacketFollows: SSHBoolean, reserved: SSHUInt32)
+  extends TransportMessage {
+  assert(messageId == TransportConstant.SSH_MSG_KEXINIT)
+  assert(cookie.length == 16)
+  override def toBytes() : Array[Byte] = toBytes(this.productIterator)
+}
+
 object TransportMessageMaker {
+  def makeNewkeys() : Newkeys = {
+    Newkeys(TransportConstant.SSH_MSG_NEWKEYS)
+  }
   def makeServiceRequest(serviceName: String) : ServiceRequest = {
     ServiceRequest(TransportConstant.SSH_MSG_SERVICE_REQUEST, SSHString(serviceName))
+  }
+  def makeKexinit(kexAlgo: Seq[String], cookie: Seq[Byte],
+  serverHostKeyAlgo: Seq[String], encC2S: Seq[String],
+  encS2C: Seq[String], macC2S: Seq[String], macS2C : Seq[String],
+  compC2S: Seq[String], compS2C: Seq[String], langC2S: Seq[String],
+  langS2C: Seq[String], firstKexPacketFollows: Boolean) : Kexinit = {
+
+    Kexinit(TransportConstant.SSH_MSG_KEXINIT, cookie.map(SSHByte(_)),
+      SSHNamedList(kexAlgo), SSHNamedList(serverHostKeyAlgo), SSHNamedList(encC2S),
+      SSHNamedList(encS2C), SSHNamedList(macC2S), SSHNamedList(macS2C),
+      SSHNamedList(compC2S), SSHNamedList(compS2C), SSHNamedList(langC2S),
+      SSHNamedList(langS2C), SSHBoolean(firstKexPacketFollows), SSHUInt32(0))
+  }
+  def makeKexinit(kexAlgo: Seq[String], 
+  serverHostKeyAlgo: Seq[String], encC2S: Seq[String],
+  encS2C: Seq[String], macC2S: Seq[String], macS2C : Seq[String],
+  compC2S: Seq[String], compS2C: Seq[String], langC2S: Seq[String],
+  langS2C: Seq[String], firstKexPacketFollows: Boolean) : Kexinit = {
+
+    val random = new SecureRandom();
+    val cookie = new Array[Byte](16)
+    random.nextBytes(cookie);
+    makeKexinit(kexAlgo, cookie, serverHostKeyAlgo, encC2S, encS2C, macC2S, macS2C, compC2S, compS2C, langC2S, langS2C,
+      firstKexPacketFollows)
   }
 }
 
 object TransportMessageParser extends ByteParsers {
 
-  lazy val serviceRequest: Parser[ServiceRequest] = byte ~ string ^^
-  {case id~name if id == TransportConstant.SSH_MSG_SERVICE_REQUEST => ServiceRequest(id, name)}
+  def messageId(id : SSHByte)  : Parser[SSHByte] = {
+    elem("messageId", (b:Byte)  => id.value.toByte == b) ^^ toSSHByte
+  }
+  lazy val newkeys : Parser[Newkeys] = messageId(TransportConstant.SSH_MSG_NEWKEYS) ^^ {(b :SSHByte)=> Newkeys(b)}
 
-  lazy val transportMessage = serviceRequest
+  lazy val serviceRequest: Parser[ServiceRequest] = messageId(TransportConstant.SSH_MSG_SERVICE_REQUEST) ~ string ^^
+  {case id~name => ServiceRequest(id, name)}
+
+  lazy val kexinit : Parser[Kexinit] = messageId(TransportConstant.SSH_MSG_KEXINIT) ~ repN(16, byte) ~ namedlist ~ namedlist ~ namedlist ~ namedlist ~ namedlist ~ namedlist ~
+  namedlist ~ namedlist ~ namedlist ~ namedlist ~ boolean ~ uint32 ^^
+  {case id~cookie~kex~hostkey~encc2s~encs2c~macc2s~macs2c~compc2s~comps2c~langc2s~langs2c~first~reserved =>
+        Kexinit(id,cookie,kex,hostkey,encc2s,encs2c,macc2s,macs2c,compc2s,comps2c,langc2s,langs2c,first,reserved)
+  }
+
+
+  lazy val transportMessage = newkeys | serviceRequest | kexinit
 
   def parse(bytes : Seq[Byte]) : ParseResult[Message] = parse[Message](transportMessage, bytes)
   def parseAll(bytes : Seq[Byte]) : ParseResult[Message] = parseAll[Message](transportMessage, bytes)
--- src/test/scala/org/unixuser/haruyama/ssh/transport/TransportMessageSuite.scala
+++ src/test/scala/org/unixuser/haruyama/ssh/transport/TransportMessageSuite.scala
@@ -4,13 +4,16 @@ import org.unixuser.haruyama.ssh.datatype._
 
 class TransportMessageSuite extends FunSuite {
     test("parse") {
+      assert(TransportMessageParser.parseAll(Array[Byte](21)).get === 
+        TransportMessageMaker.makeNewkeys())
       assert(TransportMessageParser.parseAll(Array[Byte](5, 00, 00, 00, 12, 115, 115, 104, 45, 117, 115, 101, 114, 97, 117, 116, 104)).get === 
-        ServiceRequest(SSHByte(5), SSHString("ssh-userauth")))
+        TransportMessageMaker.makeServiceRequest("ssh-userauth"))
       assert(TransportMessageParser.parseAll(Array[Byte](5, 00, 00, 00, 00)).get === 
-        ServiceRequest(SSHByte(5), SSHString("")))
+        TransportMessageMaker.makeServiceRequest(""))
     }
 
     test("ServiceRequest toBytes") {
+      assert(TransportMessageMaker.makeNewkeys().toBytes === Array[Byte](21))
       assert(TransportMessageMaker.makeServiceRequest("ssh-userauth").toBytes ===
         Array[Byte](5, 00, 00, 00, 12, 115, 115, 104, 45, 117, 115, 101, 114, 97, 117, 116, 104))
     }
