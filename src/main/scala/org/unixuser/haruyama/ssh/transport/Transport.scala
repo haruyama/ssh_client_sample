61d7d808118ec79e491549037c1b74b79d0381d9
HARUYAMA Seigo <haruyama@unixuser.org>
2011-07-01 17:07:43 +0900

    add EncryptedTransport

--- src/main/scala/org/unixuser/haruyama/ssh/transport/Transport.scala
+++ src/main/scala/org/unixuser/haruyama/ssh/transport/Transport.scala
@@ -5,12 +5,23 @@ import org.unixuser.haruyama.ssh.message.Message
 import scala.collection.mutable.ArrayBuffer
 import java.security.SecureRandom
 import java.io._
+import ch.ethz.ssh2.crypto.KeyMaterial
+import ch.ethz.ssh2.crypto.cipher.BlockCipher
+import ch.ethz.ssh2.crypto.cipher.BlockCipherFactory
+import ch.ethz.ssh2.crypto.digest.MAC
+import java.math.BigInteger
 
-abstract class Transport(parser: TransportMessageParser) {
+abstract class Transport(i: InputStream, o: OutputStream, p: TransportMessageParser) {
+  val in  = new BufferedInputStream(i)
+  val out = new BufferedOutputStream(o)
+  var parser = p
+  var recvSeqNumber : Int
+  var sendSeqNumber : Int
 
-  protected def recvRawMessage() : Array[Byte]
+  protected def recvMessageBytes() : Array[Byte]
+
   def recvMessage() : Message = {
-    val bytes : Array[Byte] = recvRawMessage()
+    val bytes : Array[Byte] = recvMessageBytes()
     val result = parser.parseAll(bytes)
     if (!result.successful) {
       throw new RuntimeException
@@ -18,46 +29,17 @@ abstract class Transport(parser: TransportMessageParser) {
     result.get
   }
 
-  protected def sendMessage(message: Array[Byte])
+  protected def sendMessageBytes(bytes: Array[Byte])
   def sendMessage(message: Message) {
-    sendMessage(message.toBytes)
+    sendMessageBytes(message.toBytes)
   }
 
-  def parseLength(bytes: Array[Byte]) : Int = {
+  protected def parseLength(bytes: Array[Byte]) : Int = {
     val l = ((bytes(0) & 0xff).toLong << 24) + ((bytes(1) & 0xff).toLong << 16) + ((bytes(2) & 0xff).toLong << 8) + (bytes(3) & 0xff).toLong
     l.toInt
   }
-}
 
-class UnencryptedTransport(in: InputStream, out: OutputStream, parser: TransportMessageParser) extends Transport(parser) {
-  val bin  = new BufferedInputStream(in)
-  val bout = new BufferedOutputStream(out)
-
-
-  override def recvRawMessage() : Array[Byte] = {
-    val lengthBytes = new Array[Byte](4)
-    if (bin.read(lengthBytes, 0, 4) == -1) {
-      throw new RuntimeException
-    }
-    val length = parseLength(lengthBytes)
-    val padding_length = bin.read
-    if (padding_length == -1) {
-      throw new RuntimeException
-    }
-    val message = new Array[Byte](length - padding_length - 1)
-
-    if (bin.read(message, 0, length - padding_length - 1) == -1) {
-      throw new RuntimeException
-    }
-    val padding = new Array[Byte](padding_length)
-
-    if (bin.read(padding, 0, padding_length) == -1) {
-      throw new RuntimeException
-    }
-    message
-  }
-
-  override def sendMessage(message: Array[Byte]) {
+  protected def packPayload(message: Array[Byte]) : Array[Byte] = {
     val remainder = (1 + message.size + 4) % 16
 
     val padding_length =
@@ -67,20 +49,132 @@ class UnencryptedTransport(in: InputStream, out: OutputStream, parser: Transport
         16 - remainder
       }
 
-      val packet_length = message.size + padding_length + 1
-      val arrayBuffer = new ArrayBuffer[Byte](4 + packet_length)
+    val packet_length = message.size + padding_length + 1
+    val arrayBuffer = new ArrayBuffer[Byte](4 + packet_length)
 
-      val random = new SecureRandom
-      val padding = new Array[Byte](padding_length)
-      random.nextBytes(padding);
+    val random = new SecureRandom
+    val padding = new Array[Byte](padding_length)
+    random.nextBytes(padding);
 
-      arrayBuffer ++= SSHUInt32(packet_length).toBytes
-      arrayBuffer += ((padding_length) & 0xff).toByte
-      arrayBuffer ++= message
-      arrayBuffer ++= padding
+    arrayBuffer ++= SSHUInt32(packet_length).toBytes
+    arrayBuffer += ((padding_length) & 0xff).toByte
+    arrayBuffer ++= message
+    arrayBuffer ++= padding
 
-      val bytes = arrayBuffer.toArray
-      bout.write(bytes, 0, bytes.length)
-      bout.flush
+    arrayBuffer.toArray
   }
 }
+
+class UnencryptedTransport(i: InputStream, o: OutputStream, p: TransportMessageParser) extends Transport(i, o, p) {
+
+  var recvSeqNumber = 0
+  var sendSeqNumber = 0
+
+  override def recvMessageBytes() : Array[Byte] = {
+    val lengthBytes = new Array[Byte](4)
+    if (in.read(lengthBytes, 0, 4) == -1) {
+      throw new RuntimeException
+    }
+    val length = parseLength(lengthBytes)
+    val padding_length = in.read
+    if (padding_length == -1) {
+      throw new RuntimeException
+    }
+    val message = new Array[Byte](length - padding_length - 1)
+
+    if (in.read(message, 0, length - padding_length - 1) == -1) {
+      throw new RuntimeException
+    }
+    val padding = new Array[Byte](padding_length)
+
+    if (in.read(padding, 0, padding_length) == -1) {
+      throw new RuntimeException
+    }
+    recvSeqNumber += 1
+    message
+  }
+
+  override def sendMessageBytes(bytes: Array[Byte]) {
+    val packet = packPayload(bytes)
+    out.write(packet)
+    out.flush
+    sendSeqNumber += 1
+  }
+}
+
+class EncryptedTransport(i: InputStream, o: OutputStream, p: TransportMessageParser, sessionId: Array[Byte], h : Array[Byte], k :
+  BigInteger, oldTransport : Transport ) extends Transport(i, o, p) {
+  // 鍵の再生成の際には古いsessionIdが存在するが， この実装では利用しない
+  // この実装では暗号とMACは決め打ちなのでサイズも決め打ち
+  val km = KeyMaterial.create("SHA1", h, k, sessionId, 16, 16, 20, 16, 16, 20)
+  val cipherC2S = BlockCipherFactory.createCipher("aes128-ctr", true, km.enc_key_client_to_server, km.initial_iv_client_to_server)
+  val cipherS2C = BlockCipherFactory.createCipher("aes128-ctr", true, km.enc_key_server_to_client, km.initial_iv_server_to_client)
+  val macC2S    = new MAC("hmac-sha1", km.integrity_key_client_to_server)
+  val macS2C    = new MAC("hmac-sha1", km.integrity_key_server_to_client)
+
+  var recvSeqNumber = oldTransport.recvSeqNumber
+  var sendSeqNumber = oldTransport.sendSeqNumber
+
+  override def recvMessageBytes() : Array[Byte] = {
+    val buf = new Array[Byte](16)
+    if (in.read(buf, 0, 16) == -1) {
+      throw new RuntimeException
+    }
+    val firstDecrptedBlock = new Array[Byte](16)
+
+    cipherS2C.transformBlock(buf, 0, firstDecrptedBlock, 0)
+
+    val length = parseLength(firstDecrptedBlock) + 4
+    var offset = 16
+    val decryptedPacket = new Array[Byte](length)
+    firstDecrptedBlock.copyToArray(decryptedPacket, 0)
+
+    while (offset < length) {
+      if (in.read(buf, 0, 16) == -1) {
+        throw new RuntimeException
+      }
+      cipherS2C.transformBlock(buf, 0, decryptedPacket, offset)
+      offset += 16
+    }
+
+    val sentMac = new Array[Byte](20)
+    if (in.read(sentMac, 0, 20) == -1) {
+        throw new RuntimeException
+    }
+
+
+    val mac = new Array[Byte](20)
+    macS2C.initMac(recvSeqNumber)
+    macS2C.update(decryptedPacket, 0, decryptedPacket.length)
+    macS2C.getMac(mac, 0)
+    assert(mac sameElements sentMac)
+    recvSeqNumber += 1
+
+    val message = new Array[Byte](length - 5 - decryptedPacket(4))
+    Array.copy(decryptedPacket, 5, message, 0, length -5 - decryptedPacket(4))
+    message
+  }
+
+
+  override def sendMessageBytes(bytes: Array[Byte]) {
+    val packet = packPayload(bytes)
+    val mac = new Array[Byte](20)
+    macC2S.initMac(sendSeqNumber)
+    macC2S.update(packet, 0, packet.length)
+    macC2S.getMac(mac, 0)
+    sendSeqNumber += 1
+
+    val encrypted = new Array[Byte](packet.length)
+    var offset = 0
+
+    while (offset < packet.length) {
+      cipherC2S.transformBlock(packet, offset, encrypted, offset)
+      offset += 16
+    }
+
+    out.write(encrypted)
+    out.write(mac)
+    out.flush
+  }
+}
+
