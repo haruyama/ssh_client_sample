f8fbfbc1629a57d48adc85460f078f3b9d12cbd5
HARUYAMA Seigo <haruyama@unixuser.org>
2011-07-01 15:08:19 +0900

    remove unnecessary spaces.

--- src/test/scala/org/unixuser/haruyama/ssh/transport/TransportMessageSuite.scala
+++ src/test/scala/org/unixuser/haruyama/ssh/transport/TransportMessageSuite.scala
@@ -4,11 +4,11 @@ import org.unixuser.haruyama.ssh.datatype._
 
 class TransportMessageSuite extends FunSuite {
     test("parse") {
-      assert((new TransportMessageParser).parseAll(Array[Byte](21)).get === 
+      assert((new TransportMessageParser).parseAll(Array[Byte](21)).get ===
         TransportMessageMaker.makeNewkeys())
-      assert((new TransportMessageParser).parseAll(Array[Byte](5, 00, 00, 00, 12, 115, 115, 104, 45, 117, 115, 101, 114, 97, 117, 116, 104)).get === 
+      assert((new TransportMessageParser).parseAll(Array[Byte](5, 00, 00, 00, 12, 115, 115, 104, 45, 117, 115, 101, 114, 97, 117, 116, 104)).get ===
         TransportMessageMaker.makeServiceRequest("ssh-userauth"))
-      assert((new TransportMessageParser).parseAll(Array[Byte](5, 00, 00, 00, 00)).get === 
+      assert((new TransportMessageParser).parseAll(Array[Byte](5, 00, 00, 00, 00)).get ===
         TransportMessageMaker.makeServiceRequest(""))
     }
 
