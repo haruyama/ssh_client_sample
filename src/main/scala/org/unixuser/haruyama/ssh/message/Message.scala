c36a1c97f213dc07301b9afa931380a2750c65dc
HARUYAMA Seigo <haruyama@unixuser.org>
2011-08-11 11:04:45 +0900

    use ArrayBuilder instead of ArrayBuffer.

--- src/main/scala/org/unixuser/haruyama/ssh/message/Message.scala
+++ src/main/scala/org/unixuser/haruyama/ssh/message/Message.scala
@@ -1,26 +1,25 @@
 package org.unixuser.haruyama.ssh.message
 
 import org.unixuser.haruyama.ssh.datatype.SSHDataType
-import scala.collection.mutable.ArrayBuffer
+//import scala.collection.mutable.ArrayBuilder
+import scala.collection.mutable.ArrayBuilder
 
 trait Message extends Product {
   def toBytes() : Array[Byte] = toBytes(this.productIterator)
 
   private def toBytes(s : TraversableOnce[Any]) : Array[Byte] = {
-    val arrayBuffer = new ArrayBuffer[Byte]
-    addArrayBuffer(s, arrayBuffer)
-    arrayBuffer.toArray
+    val arrayBuilder = ArrayBuilder.make[Byte]
+    addArrayBuilder(s, arrayBuilder)
+    arrayBuilder.result
   }
 
-  private def addArrayBuffer(s : TraversableOnce[Any], arrayBuffer : ArrayBuffer[Byte]) {
-    s.foreach { e =>
-      e match {
-        case t: SSHDataType => {
-          arrayBuffer ++= t.toBytes
-        }
-        case to: TraversableOnce[_] => addArrayBuffer(to, arrayBuffer)
-        case _ => throw new IllegalArgumentException
+  private def addArrayBuilder(s : TraversableOnce[Any], arrayBuilder : ArrayBuilder[Byte]) {
+    s.foreach {
+      case t: SSHDataType => {
+        arrayBuilder ++= t.toBytes
       }
+      case to: TraversableOnce[_] => addArrayBuilder(to, arrayBuilder)
+      case _ => throw new IllegalArgumentException
     }
   }
 }
